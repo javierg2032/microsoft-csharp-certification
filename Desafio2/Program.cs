168e8b5f0e1fd58bff95069ebbbf02b860d4dd06
Javier Galán Cortés <javier.galan2032@gmail.com>
2026-01-13 21:13:57 +0100

    Rename Desafio to Desafio1 and add Desafio2 project
    Renamed the Desafio project and source files to Desafio1. Added a new Desafio2 project with a Program.cs that prints a personalized message including name, message count, and temperature.

---
+++ Desafio2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Write code in the C# Code Editor to display the following message: Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+
+string name = "Bob";
+int num = 3;
+float temp = 34.4F;
+
+Console.WriteLine("Hello, " + name + "! You have " + num + " messages in your inbox. The temperature is " + temp + " celsius.");
