acff95f5beb9efdce682f85c95ca00dc85656249
Javier Galán Cortés <javier.galan2032@gmail.com>
2026-01-13 19:59:12 +0100

    Add projects for variable declaration and literals
    Added two new C# console projects: DeclararVariables, demonstrating variable assignment and type errors, and ValoresLiterales, showcasing usage of literal values and their types. Both projects target .NET 8.0.

---
+++ DeclararVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿string firstName = "Javier";
+Console.WriteLine(firstName); //Javier
+
+/*
+int lastName="Galán";
+Console.WriteLine(lastName); //error CS0029: No se puede convertir implícitamente el tipo 'string' en 'int'
+*/
+
+firstName = "Jose";
+Console.WriteLine(firstName);
+firstName = "Amaya";
+Console.WriteLine(firstName);
+firstName = "Enrique";
+Console.WriteLine(firstName);
---
+++ ValoresLiterales/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine('b'); //b
+//Console.WriteLine('Hello World!');//error CS1012: Demasiados caracteres en literal de carácter
+Console.WriteLine(123); //123
+Console.WriteLine(0.25F); //0,25
+Console.WriteLine(2.625); //2,625
+Console.WriteLine(12.39816m); //12,39816
+Console.WriteLine(true); //True
+Console.WriteLine(false); //False
+
+Console.WriteLine(true.GetType().Name);
+Console.WriteLine("true".GetType().Name);
+Console.WriteLine(123.GetType().Name);
+Console.WriteLine("123".GetType().Name);
