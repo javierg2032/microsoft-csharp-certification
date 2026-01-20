fd8dbe1e899248938cb4098b4e2442999fa3d211
Javier Galán Cortés <javier.galan2032@gmail.com>
2026-01-20 17:06:53 +0100

    Add string concatenation and formatting examples
    Added two new projects: ConcatenacionDeCadenas demonstrates string concatenation techniques, and FormatoCadenasLiterales covers string formatting, escape sequences, and verbatim string literals in C#. Updated the solution file to include these projects.

---
+++ ConcatenacionDeCadenas/Program.cs
@@ -0,0 +1,15 @@
+﻿/*<------Concatenación de una cadena de literales y una variable------>*/
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message); //Hello Bob
+
+/*<------Concatenación de varias variables y cadenas de literales------>*/
+string firstName1 = "Bob";
+string greeting = "Hello";
+string message1 = greeting + " " + firstName1 + "!";
+Console.WriteLine(message1); //Hello Bob!
+
+/*<------Evitación de variables intermedias------>*/
+string firstName2 = "Bob";
+string greeting2 = "Hello";
+Console.WriteLine(greeting2 + " " + firstName2 + "!"); //Hello Bob!
---
+++ FormatoCadenasLiterales/Program.cs
@@ -0,0 +1,53 @@
+﻿/*------Secuencias de escape de caracteres------*/
+
+Console.WriteLine("Hello\nWorld!"); //Hello
+                                    //World!
+Console.WriteLine("Hello\tWorld!"); //Hello   World!
+Console.WriteLine("Hello \"World\"!"); //Hello "World"!
+Console.WriteLine("c:\\source\\repos"); //c:\source\repos
+
+/*------Aplicación de formato a la salida mediante secuencias de escape de caracteres------*/
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+
+/*
+Resultado:
+
+Generating invoices for customer "Contoso Corp" ...
+
+Invoice: 1021           Complete!
+Invoice: 1022           Complete!
+
+Output Directory:
+*/
+
+
+/*------Literal de cadena textual------*/
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+/*
+Resultado:
+
+c:\source\repos    
+        (this is where your code goes)
+*/
+
+
+/*------Aplicación de formato a la salida mediante literales de cadena textual------*/
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+/*
+Resultado:
+Generating invoices for customer "Contoso Corp" ...
+
+Invoice: 1021           Complete!
+Invoice: 1022           Complete!
+
+Output Directory:       c:\invoices
+*/
