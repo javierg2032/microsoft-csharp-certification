0aaac385b350dd927591c82b5651202514b2bae0
Javier Galán Cortés <javier.galan2032@gmail.com>
2026-01-29 12:42:52 +0100

    Add project to convert Fahrenheit to Celsius
    Initial commit for Desafio4 project. Includes a simple program that calculates and prints the Celsius equivalent of a hardcoded Fahrenheit temperature.

---
+++ Desafio4/Program.cs
@@ -0,0 +1,5 @@
+﻿/*<------Desafío: Calcular Celsius según la temperatura actual en Fahrenheit------>*/
+//
+int fahrenheit = 94;
+double celsius = (fahrenheit - 32) * (5.0/9.0);
+Console.Write("The temperature is "+celsius+" Celsius.");
